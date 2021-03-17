de13c990de69de020e12452b2c3ec49d17009f8d
Alexandra <andracolexacareer@gmail.com>
2021-03-17 23:21:07 +0100

    setup server
    - setup db
    - setup sequelize
    - create use model
    - created user migration

---
+++ server/database/migrations/20210317213517-create-user.js
@@ -0,0 +1,50 @@
+export default {
+  /**
+   * Run the migration
+   *
+   * @param {Sequelize.QueryInterface} queryInterface - The interface that communicates
+   * with the database.
+   * @param {Sequelize} Sequelize - Sequelize object.
+   * @return {void}
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+   /**
+   * Reverse the migration
+   *
+   * @param {Sequelize.QueryInterface} queryInterface - The interface that communicates
+   * with the database.
+   * @param {Sequelize} Sequelize - Sequelize object.
+   * @return {void}
+   */
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+};
---
+++ server/models/user.js
@@ -0,0 +1,57 @@
+import bcrypt from 'bcrypt';
+
+/**
+ * A model class representing user resource
+ *
+ * @param {Sequelize} sequelize - Sequelize object
+ * @param {Sequelize.DataTypes} DataTypes - A convinient object holding data types
+ * @return {Sequelize.Model} - User model
+*/
+export default (sequelize, DataTypes) => {
+  /**
+   * @type {Sequelize.Model}
+  */
+
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+    */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    // Model attributes are defined here
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        this.setDataValue('password', bcrypt.hashSync(value, 10));
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW,
+      onUpdate: sequelize.NOW,
+    },
+    deletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  }, {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User', // We need to choose the model name
+  });
+  return User;
+}
