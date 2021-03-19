5d983194fc466e908e5848aab6117d9b158cef60
Alexandra Collins <andracolexa@gmail.com>
2021-03-20 00:43:52 +0100

    modifiy deployment config script

---
+++ server/resolvers/blog.js
@@ -0,0 +1,20 @@
+// import models from '../models';
+
+// const { Blog } = models;
+
+// const createBlog = async (root, { title, body }) => {
+//   try {
+//     const blog = await Blog.create({
+//       title,
+//       body
+//     });
+//     return {
+//       blog,
+//       message: 'Authentication succesfull'
+//     };
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
+
+// export { createBlog };
